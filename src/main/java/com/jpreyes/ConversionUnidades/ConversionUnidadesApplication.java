fd168c770582b9fba9e5b4b7f007927d93f18eba
jpreyes <pablo16100279@gmail.com>
2025-01-19 13:09:25 -0600

    Dependencias de proyecto inicial

---
+++ src/main/java/com/jpreyes/ConversionUnidades/ConversionUnidadesApplication.java
@@ -0,0 +1,13 @@
+package com.jpreyes.ConversionUnidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversionUnidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversionUnidadesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpreyes/ConversionUnidades/ConversionUnidadesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpreyes.ConversionUnidades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionUnidadesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
